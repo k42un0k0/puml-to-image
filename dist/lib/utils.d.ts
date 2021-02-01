82097e3a120190b5f61ed6723c0308cbc18a9975
k42un0k0 <k42un0k0.put1put1@gmail.com>
2021-02-01 21:59:34 +0900

    remove dist

--- dist/lib/utils.d.ts
+++
@@ -1,3 +0,0 @@
-export declare type Callback<R = any, E = any> = (err: E, res: R) => void;
-export declare function cbToPromise<A = unknown, R = unknown, E = unknown>(fn: (arg: A, cb: Callback<R, E>) => void): (arg: A) => Promise<R>;
-export declare const IS_WINDOWS: boolean;
