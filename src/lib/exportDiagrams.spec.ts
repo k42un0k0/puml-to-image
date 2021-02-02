5fffe7a35fcff683db60bf3324870423725c6493
k42un0k0 <k42un0k0.put1put1@gmail.com>
2021-02-03 04:43:07 +0900

    fix test

--- src/lib/exportDiagrams.spec.ts
+++ src/lib/exportDiagrams.spec.ts
@@ -1,59 +1,108 @@
-import { exportDiagrams } from "./exportDiagrams";
-import { Callback, IS_WINDOWS } from "./utils";
-import glob from "glob"
-import { exec } from "child_process"
-import { mocked } from "ts-jest/utils"
+import { IS_WINDOWS } from "./utils";
 
 jest.mock("glob", () => {
-    return jest.fn()
-})
+  return jest.fn();
+});
 jest.mock("child_process", () => {
-    return {
-        exec: jest.fn()
-    }
-})
+  return {
+    exec: jest.fn(),
+  };
+});
 
+function importStatement() {
+  const glob = require("glob");
+  const { exec } = require("child_process");
+  const { mocked } = require("ts-jest/utils");
+  const { exportDiagrams } = require("./exportDiagrams");
+
+  return {
+    glob,
+    exec,
+    mocked,
+    exportDiagrams,
+  };
+}
 describe("exportDiagrams", () => {
-    mocked(exec).mockImplementation((arg, cb: any) => {
-        return cb(null, "exec called");
-    })
-    beforeEach(() => {
-        mocked(glob).mockClear()
-        mocked(exec).mockClear()
-    })
-    test("call exec once and glob once", async () => {
-        mocked(glob).mockImplementation((_, cb: any) => {
-            return cb(null, ["aaa/uuu.pu"])
-        })
+  beforeEach(() => {
+    jest.resetModules();
+  });
+  if (!IS_WINDOWS)
+    describe("when linux", () => {
+      beforeEach(() => {
+        jest.mock("./utils", () => {
+          return {
+            ...jest.requireActual<{}>("./utils"),
+            IS_WINDOWS: false,
+          };
+        });
+      });
+      test("call exec once and glob once", async () => {
+        const { glob, exec, mocked, exportDiagrams } = importStatement();
+        mocked(exec).mockImplementation((_: any, cb: any) => {
+          return cb(null, "exec called");
+        });
+        mocked(glob).mockImplementation((_: any, cb: any) => {
+          return cb(null, ["aaa/uuu.pu"]);
+        });
 
+        await exportDiagrams("aaa", "eee", "./bin/hello.jar");
 
-        await exportDiagrams("aaa", "eee", "./bin/hello.jar")
+        expect(mocked(glob).mock.calls.length).toBe(1);
+        expect(mocked(glob).mock.calls[0][0]).toBe("aaa/**/*.{pu,puml}");
+        expect(mocked(exec).mock.calls.length).toBe(1);
+        expect(mocked(exec).mock.calls[0][0]).toBe(
+          "java -Dfile.encoding=UTF-8 -jar ./bin/hello.jar aaa/uuu.pu -o ../eee"
+        );
+      });
+      test("call exec twice and glob once", async () => {
+        const { glob, exec, mocked, exportDiagrams } = importStatement();
+        mocked(exec).mockImplementation((arg: any, cb: any) => {
+          return cb(null, "exec called");
+        });
+        mocked(glob).mockImplementation((_: any, cb: any) => {
+          return cb(null, ["bbb/uuu.pu", "bbb/ttt.puml"]);
+        });
+        await exportDiagrams("bbb", "eee", "./hello.jar");
 
-        expect(mocked(glob).mock.calls.length).toBe(1)
-        expect(mocked(glob).mock.calls[0][0]).toBe("aaa/**/*.{pu,puml}")
-        expect(mocked(exec).mock.calls.length).toBe(1)
-        if (IS_WINDOWS) {
-            expect(mocked(exec).mock.calls[0][0]).toBe("chcp 65001 & java -Dfile.encoding=UTF-8 -jar ./bin/hello.jar aaa/uuu.pu -o ..\\eee")
-        } else {
-            expect(mocked(exec).mock.calls[0][0]).toBe("java -Dfile.encoding=UTF-8 -jar ./bin/hello.jar aaa/uuu.pu -o ..\\eee")
-        }
-    })
-    test("call exec twice and glob once", async () => {
-        mocked(glob).mockImplementation((_, cb: any) => {
-            return cb(null, ["bbb/uuu.pu", "bbb/ttt.puml"])
-        })
-        await exportDiagrams("bbb", "eee", "./hello.jar")
+        expect(mocked(glob).mock.calls.length).toBe(1);
+        expect(mocked(glob).mock.calls[0][0]).toBe("bbb/**/*.{pu,puml}");
+        expect(mocked(exec).mock.calls.length).toBe(2);
+        expect(mocked(exec).mock.calls[0][0]).toBe(
+          "java -Dfile.encoding=UTF-8 -jar ./hello.jar bbb/uuu.pu -o ../eee"
+        );
+        expect(mocked(exec).mock.calls[1][0]).toBe(
+          "java -Dfile.encoding=UTF-8 -jar ./hello.jar bbb/ttt.puml -o ../eee"
+        );
+      });
+    });
+  if (IS_WINDOWS)
+    describe("when windows", () => {
+      beforeEach(() => {
+        jest.mock("./utils", () => {
+          return {
+            ...jest.requireActual<{}>("./utils"),
+            IS_WINDOWS: true,
+          };
+        });
+      });
+      test("call exec once and glob once", async () => {
+        const { glob, exec, mocked, exportDiagrams } = importStatement();
+        mocked(exec).mockImplementation((_: any, cb: any) => {
+          return cb(null, "exec called");
+        });
+        mocked(glob).mockImplementation((_: any, cb: any) => {
+          return cb(null, ["aaa\\uuu.pu"]);
+        });
 
-        expect(mocked(glob).mock.calls.length).toBe(1)
-        expect(mocked(glob).mock.calls[0][0]).toBe("bbb/**/*.{pu,puml}")
-        expect(mocked(exec).mock.calls.length).toBe(2)
-        if (IS_WINDOWS) {
-            expect(mocked(exec).mock.calls[0][0]).toBe("chcp 65001 & java -Dfile.encoding=UTF-8 -jar ./hello.jar bbb/uuu.pu -o ..\\eee")
-            expect(mocked(exec).mock.calls[1][0]).toBe("chcp 65001 & java -Dfile.encoding=UTF-8 -jar ./hello.jar bbb/ttt.puml -o ..\\eee")
-        } else {
-            expect(mocked(exec).mock.calls[0][0]).toBe("java -Dfile.encoding=UTF-8 -jar ./hello.jar bbb/uuu.pu -o ..\\eee")
-            expect(mocked(exec).mock.calls[1][0]).toBe("java -Dfile.encoding=UTF-8 -jar ./hello.jar bbb/ttt.puml -o ..\\eee")
+        await exportDiagrams("aaa", "eee", "./bin/hello.jar");
 
-        }
-    })
-})
+        expect(mocked(glob).mock.calls.length).toBe(1);
+        expect(mocked(glob).mock.calls[0][0]).toBe("aaa/**/*.{pu,puml}");
+        expect(mocked(exec).mock.calls.length).toBe(1);
+
+        expect(mocked(exec).mock.calls[0][0]).toBe(
+          "chcp 65001 & java -Dfile.encoding=UTF-8 -jar ./bin/hello.jar aaa\\uuu.pu -o ..\\eee"
+        );
+      });
+    });
+});
--- src/lib/exportDiagrams.ts
+++ src/lib/exportDiagrams.ts
@@ -1,70 +1,83 @@
-import chalk from "chalk"
-import glob from "glob"
-import { exec } from "child_process"
-import { cbToPromise, IS_WINDOWS } from "./utils"
-import path from "path"
+import chalk from "chalk";
+import glob from "glob";
+import { exec } from "child_process";
+import { cbToPromise, IS_WINDOWS } from "./utils";
+import path from "path";
 
 const pGlob = cbToPromise<string, string[]>(glob);
 const pExec = cbToPromise<string, string>(exec);
 
 const EXECUTE_PROCESS_PATH = "./";
 
-export async function exportDiagrams(inputDirName: string, outputDirName: string, jarFilePath: string) {
+export async function exportDiagrams(
+  inputDirName: string,
+  outputDirName: string,
+  jarFilePath: string
+) {
+  console.log(chalk.green("export diagrams"));
 
-    console.log(chalk.green('export diagrams'));
+  let files: string[] = [];
+  try {
+    // inputDirNameから*.puを検索
+    files = await findAllPuml(inputDirName);
+  } catch (error) {
+    exitWithShowError(1, error);
+  }
 
-    let files: string[] = [];
-    try {
-        // inputDirNameから*.puを検索
-        files = await findAllPuml(inputDirName);
-    } catch (error) {
-        exitWithShowError(1, error);
-    }
+  // すべての.pu を全て画像化
+  const promises = files.map(
+    pumlToImage(inputDirName, outputDirName, jarFilePath)
+  );
 
-    // すべての.pu を全て画像化
-    const promises = files.map(pumlToImage(inputDirName, outputDirName, jarFilePath));
-
-    try {
-        await Promise.all(promises);
-    } catch (error) {
-        exitWithShowError(2, error);
-    }
-    console.log(chalk.green('Done!!'));
+  try {
+    await Promise.all(promises);
+  } catch (error) {
+    exitWithShowError(2, error);
+  }
+  console.log(chalk.green("Done!!"));
+  return IS_WINDOWS;
 }
 
 function exitWithShowError(code: number, err: Error) {
-    console.log(chalk.red(err));
-    process.exit(code);
+  console.log(chalk.red(err));
+  process.exit(code);
 }
 
 function findAllPuml(rootDir: string) {
-    console.log("find puml files");
-    let globPattern = "**/*.{pu,puml}";
-    if (rootDir) {
-        globPattern = `${rootDir}/${globPattern}`;
-    }
-    return pGlob(globPattern);
+  console.log("find puml files");
+  let globPattern = "**/*.{pu,puml}";
+  if (rootDir) {
+    globPattern = `${rootDir}/${globPattern}`;
+  }
+  return pGlob(globPattern);
 }
 
-function pumlToImage(fromDirName: string, toDirName: string, jarFilePath: string) {
-    console.log(`export image to ${toDirName}`);
+function pumlToImage(
+  fromDirName: string,
+  toDirName: string,
+  jarFilePath: string
+) {
+  console.log(`export image to ${toDirName}`);
 
-    return function (file: string) {
-        // 相対pathで出力用フォルダを指定する
-        function generateOutputPath() {
-            const fileDirPath = path.dirname(file);
-            return path.join(path.relative(fileDirPath, EXECUTE_PROCESS_PATH), fileDirPath.replace(RegExp(fromDirName, "g"), toDirName));
-        }
-        const outputPath = generateOutputPath()
-        console.log(`from ${file}`);
-        let javaCommand = `java -Dfile.encoding=UTF-8 -jar ${jarFilePath} ${file} -o ${outputPath}`
-        // windowsの場合、出力をutf-8に変換
-        if (IS_WINDOWS) {
-            javaCommand = "chcp 65001 & " + javaCommand
-        }
-        return pExec(javaCommand).then((res) => {
-            console.log(`output of ${file}: ` + res);
-            return res;
-        })
+  return function (file: string) {
+    // 相対pathで出力用フォルダを指定する
+    function generateOutputPath() {
+      const fileDirPath = path.dirname(file);
+      return path.join(
+        path.relative(fileDirPath, EXECUTE_PROCESS_PATH),
+        fileDirPath.replace(RegExp(fromDirName, "g"), toDirName)
+      );
     }
+    const outputPath = generateOutputPath();
+    console.log(`from ${file}`);
+    let javaCommand = `java -Dfile.encoding=UTF-8 -jar ${jarFilePath} ${file} -o ${outputPath}`;
+    // windowsの場合、出力をutf-8に変換
+    if (IS_WINDOWS) {
+      javaCommand = "chcp 65001 & " + javaCommand;
+    }
+    return pExec(javaCommand).then((res) => {
+      console.log(`output of ${file}: ` + res);
+      return res;
+    });
+  };
 }
